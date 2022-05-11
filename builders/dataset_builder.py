5673ad57ce47ab41423f9af68de478c0013df563
XU-GITHUB-curry <55744980+XU-GITHUB-curry@users.noreply.github.com>
2022-05-11 11:12:06 +0800

    Update dataset_builder.py

--- builders/dataset_builder.py
+++ builders/dataset_builder.py
@@ -2,7 +2,7 @@ import os
 import pickle #该pickle模块实现了用于序列化和反序列化Python对象结构的二进制协议
 #python的pickle模块实现了基本的数据序列和反序列化。通过pickle模块的序列化操作我们能够将程序中运行的对象信息保存到文件中去，永久存储；
 #通过pickle模块的反序列化操作，我们能够从文件中创建上一次程序保存的对象
-from torch.utils import data #自由数据的读取
+from torch.utils import data
 from dataset.cityscapes import CityscapesDataSet, CityscapesTrainInform, CityscapesValDataSet, CityscapesTestDataSet
 from dataset.camvid import CamVidDataSet, CamVidValDataSet, CamVidTrainInform, CamVidTestDataSet
 
